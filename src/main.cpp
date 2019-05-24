663460e06c4e111444d38a5d4121ffc90c77337e
Tony DUCROCQ <tony.ducrocq@worldline.com>
2019-05-24 16:53:12 +0200

    handle more than one device

--- src/main.cpp
+++ src/main.cpp
@@ -22,16 +22,19 @@ static BLEUUID serviceUUID("226c0000-6476-4566-7562-66734470666d");
 // The characteristic of the remote service we are interested in.
 static BLEUUID    charUUID("226caa55-6476-4566-7562-66734470666d");
 
+static BLEAddress* pServerAddresses[20];
+
 static BLEAddress *pServerAddress;
 static BLEClient*  pClient;
 static boolean doConnect = false;
 static boolean connected = false;
 static BLERemoteCharacteristic* pRemoteCharacteristic;
+static int count=0;
+static int current=0;
 
 static double temperature;
 static double humidity;
 static unsigned long lastUpdate = 0;
-static int i = 0;
 
 void IRAM_ATTR resetModule() {
   ets_printf("REBOOT\n");
@@ -51,24 +54,31 @@ static void notifyCallback(
     temperature = data.substring(2, 6).toDouble();
     humidity = data.substring(9, 13).toDouble();
 
+    Serial.println(pBLERemoteCharacteristic->toString().c_str());
     Serial.printf("%.1f / %.1f\n", temperature, humidity);
 
     lastUpdate = millis();
     // Disconnect from BT
     pClient->disconnect();
     connected = false;
-    doConnect = false;
+
+    if (current < count) {
+      doConnect = true;
+    }
+
+
 }
 
-bool connectToServer(BLEAddress pAddress) {
+bool connectToServer(BLEAddress *pAddress) {
+
     Serial.print("Forming a connection to ");
-    Serial.println(pAddress.toString().c_str());
+    Serial.println(pAddress->toString().c_str());
 
     pClient  = BLEDevice::createClient();
     Serial.println(" - Created client");
 
     // Connect to the remove BLE Server.
-    pClient->connect(pAddress);
+    pClient->connect(*pAddress);
     Serial.println(" - Connected to server");
 
     // Obtain a reference to the service we are after in the remote BLE server.
@@ -102,31 +112,38 @@ class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
    * Called for each advertising BLE server.
    */
   void onResult(BLEAdvertisedDevice advertisedDevice) {
-    Serial.print("BLE Advertised Device found: ");
-    Serial.println(advertisedDevice.toString().c_str());
+    Serial.print("BLE Advertised Device found");
 
     // We have found a device, let us now see if it contains the service we are looking for.
     if (advertisedDevice.haveServiceUUID() && advertisedDevice.getServiceUUID().equals(advertisedUUID)) {
 
       //
-      Serial.print("Found our device!  address: ");
-      advertisedDevice.getScan()->stop();
+      Serial.print("Found our device!  address");
+      //advertisedDevice.getScan()->stop();
 
-      pServerAddress = new BLEAddress(advertisedDevice.getAddress());
-      doConnect = true;
-
+      //pServerAddress = new BLEAddress(advertisedDevice.getAddress());
+      pServerAddresses[count++] = new BLEAddress(advertisedDevice.getAddress());
+      //doConnect = true;
+      Serial.println(advertisedDevice.toString().c_str());
     } // Found our server
   } // onResult
 }; // MyAdvertisedDeviceCallbacks
 
+static void scanCompleteCB(BLEScanResults scanResults) {
+  Serial.println("Scan complete");
+  for (int i=0; i<count; i++) {
+    Serial.println(pServerAddresses[i]->toString().c_str());
+  }
+  doConnect = true;
+}
 
 void setup() {
   Serial.begin(115200);
 
   lastUpdate = millis();
 
-  WiFiManager wifiManager;
-  wifiManager.autoConnect("MijiaBleBridge");
+  //WiFiManager wifiManager;
+  //wifiManager.autoConnect("MijiaBleBridge");
 
   Serial.println("Starting Arduino BLE Client application...");
   BLEDevice::init("");
@@ -134,10 +151,12 @@ void setup() {
   // Retrieve a Scanner and set the callback we want to use to be informed when we
   // have detected a new device.  Specify that we want active scanning and start the
   // scan to run for 30 seconds.
+  count = 0;
+  current = 0;
   BLEScan* pBLEScan = BLEDevice::getScan();
   pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks());
   pBLEScan->setActiveScan(true);
-  pBLEScan->start(30);
+  pBLEScan->start(30, scanCompleteCB);
 } // End of setup.
 
 
@@ -147,21 +166,23 @@ void loop() {
   // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
   // connected we set the connected flag to be true.
   if (doConnect) {
-    if (connectToServer(*pServerAddress)) {
+    doConnect = false;
+    if (connectToServer(pServerAddresses[current])) {
       Serial.println("We are now connected to the BLE Server.");
+      current++;
       connected = true;
     } else {
       Serial.println("We have failed to connect to the server; there is nothin more we will do.");
     }
-    doConnect = false;
+
 
   }
 
   delay(1000); // Delay a second between loops.
   if (millis() - lastUpdate  > UPDATE_TIME) {
-    doConnect = true;
+    //doConnect = true;
   }
   if (millis() - lastUpdate > UPDATE_LIMIT) {
-    resetModule();
+    //resetModule();
   }
 } // End of loop
